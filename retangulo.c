c33f3006a3f925343ff6bdd01b33678a03d7e50f
kaiofabiano <kaiofcc.kf@gmail.com>
2015-11-02 22:57:44 +0000

    Envio do trabalho 2

---
+++ retangulo.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+/* 
+    - Trabalho 2 de Programacao de Computadores I - Retangulo
+    - Curso de Sistemas de Informacao
+    
+    - Aluno: Kaio Fabiano  -  Matricula: 0050012859
+    
+    - Professor: Alex Salgado
+*/
+
+
+int main()
+{
+    
+    int resposta, altura, largura, i, j;
+    char nome[20], continuar;
+    
+    do
+    {
+        printf("Informe o seu nome: ");
+        scanf("%s", nome);
+    
+    
+        printf("\nSimbolo do pixel (1 para %c, 2 para %c, 3 para %c): ", 42, 64, 43);
+        scanf("%d", &resposta);
+        
+        while(resposta<1 || resposta>3)
+        {
+            printf("\nDesculpe...Escolha um simbolo valido (1 para %c, 2 para %c, 3 para %c): ", 42, 64, 43);
+            scanf("%d", &resposta);
+        }
+    
+    
+        printf("\nTamanho da LARGURA (lembrando, deve ser maior que 2): ");
+        scanf("%d",&largura);
+        
+        while(largura<3)
+        {
+            printf("\nDesculpe...Para desenhar o retangulo a LARGURA deve ser maior que 2. Tente novamente: ");
+            scanf("%d",&largura);
+        }
+        
+        
+        printf("\nTamanho da ALTURA do seu Retangulo (lembrando, deve ser maior que 2: ");
+        scanf("%d",&altura);
+        
+        while(altura<3)
+        {
+            printf("\nDesculpe...Para desenhar o retangulo a LARGURA deve ser maior que 2. Tente novamente:\n");
+            scanf("%d",&altura);
+        }
+
+
+        printf("\n\nOla, meu nome eh %s e o retangulo ficou assim:\n\n", nome);
+        for (i=1; i<=altura; i++)
+        {
+            for(j=1; j<=largura; j++)
+            {
+                if (i>1 && j>1 && j<largura && i<altura)
+                {
+                    printf(" ");
+                }
+                
+                else
+                {
+                    if (resposta == 1)
+                    {    
+                        printf("%c",42);
+                    }
+                        
+                    if (resposta == 2)
+                    {    
+                        printf("%c",64);
+                    }
+                        
+                    if (resposta == 3)
+                    {    
+                        printf("%c",43);
+                    }
+                }
+            }
+        printf("\n");
+        }
+        
+        printf("\nDeseja continuar (s/n)?\n");
+        scanf(" %c", &continuar);
+    }
+    while(continuar != 'n');
+    return 0;
+}
